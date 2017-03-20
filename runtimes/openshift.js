a6ebb949291d0e2b813ac5768b1191d97ae2f3f8
Mike Evans <mike.evans@numical.com>
2017-03-20 20:44:12 +0000

    set to cartridge supplied port

--- runtimes/openshift.js
+++ runtimes/openshift.js
@@ -1,3 +1,4 @@
 const messageReceiver = require('../lib/sms/messageReceiver.js');
-const port = process.env.OPENSHIFT_NODEJS_PORT || 1971;
+// const port = process.env.OPENSHIFT_NODEJS_PORT || 1971;
+const port = process.env.NODE_PORT || 1971;
 messageReceiver.start(port);
