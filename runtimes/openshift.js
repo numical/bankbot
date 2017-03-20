9df80b5911d2297d7b5a6b5b0bd1f402d12885cb
Mike Evans <mike.evans@numical.com>
2017-03-20 21:04:00 +0000

    more startup info

--- runtimes/openshift.js
+++ runtimes/openshift.js
@@ -1,4 +1,5 @@
 const messageReceiver = require('../lib/sms/messageReceiver.js');
 // const port = process.env.OPENSHIFT_NODEJS_PORT || 1971;
 const port = process.env.NODE_PORT || 1971;
-messageReceiver.start(port);
+const hostname = process.env.NODE_IP || 'localhost';
+messageReceiver.start(port, hostname);
