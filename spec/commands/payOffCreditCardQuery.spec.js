a32098c1aa0caa9485c1ece3692319ea0b9235c8
Mike Evans <mike.evans@numical.com>
2017-03-27 20:20:27 +0100

    debug schedule issue

--- spec/commands/payOffCreditCardQuery.spec.js
+++ spec/commands/payOffCreditCardQuery.spec.js
@@ -1,32 +1,49 @@
 /* eslint-env mocha */
 /* eslint no-unused-expressions : 0 */
 require('../initialisedChai.js');
-const proxyquire = require('proxyquire').noCallThru();
+const proxyquire = require('proxyquire').noPreserveCache().noCallThru();
 const sinon = require('sinon');
 const { content } = require('../../lib/commands/payOffCreditCardQuery.js');
 
-const state = { user: { number: 'TEST NUMBER' } };
-const getState = sinon.stub().returns(Promise.resolve(state));
 const sendMessage = sinon.spy();
-const subject = proxyquire('../../lib/commands/payOffCreditCardQuery.js', {
-  '../services/persistence.js': { getState },
-  '../channels/sms/messageSender.js': sendMessage
-});
-const replyContext = { foo: 'bar' };
+const replyContext = { number: 'TEST NUMBER' };
 
-describe.only('pay off credit card query command', () => {
+describe('pay off credit card query command', () => {
   beforeEach(() => {
-    getState.reset();
     sendMessage.reset();
   });
 
-  it('calls persistence with passed reply context', async () => {
-    await subject(replyContext);
-    getState.calledWithExactly(replyContext);
+  describe('with persistence mocked', () => {
+    const state = { user: { number: 'MOCKED TEST NUMBER' } };
+    const getState = sinon.stub().returns(Promise.resolve(state));
+    const subject = proxyquire('../../lib/commands/payOffCreditCardQuery.js', {
+      '../services/persistence.js': { getState },
+      '../channels/sms/messageSender.js': sendMessage
+    });
+
+    beforeEach(() => {
+      getState.reset();
+    });
+
+    it('calls persistence with passed reply context', async () => {
+      await subject(replyContext);
+      getState.calledWithExactly(replyContext);
+    });
+
+    it('calls send message with user number and message', async () => {
+      await subject(replyContext);
+      sendMessage.calledWithExactly('MOCKED TEST NUMBER', content);
+    });
   });
 
-  it('calls send message with user number and message', async () => {
-    await subject(replyContext);
-    sendMessage.calledWithExactly('TEST NUMBER', content);
+  describe.only('with real persistence', () => {
+    const subject = proxyquire('../../lib/commands/payOffCreditCardQuery.js', {
+      '../channels/sms/messageSender.js': sendMessage
+    });
+
+    it('calls send with user number and message', async () => {
+      await subject(replyContext);
+      sendMessage.calledWithExactly('TEST NUMBER', content);
+    });
   });
 });
