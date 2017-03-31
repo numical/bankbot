ba38ecf90c2f9d2e8ca8e0878640aef90b479600
Mike Evans <mike.evans@numical.com>
2017-03-31 08:36:42 +0100

    switch data to js form

---
+++ data/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'Bob',
+    accounts: {
+      current: {
+        balance: '102.82'
+      },
+      creditcard: {
+        balance: '1480.00',
+        date: '12'
+      }
+    }
+  },
+  {
+    name: 'Alice',
+    accounts: {
+      current: {
+        balance: '740.33'
+      },
+      creditcard: {
+        balance: '199.99',
+        date: '16'
+      }
+    }
+  }
+];
