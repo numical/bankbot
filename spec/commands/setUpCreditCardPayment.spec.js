7b1e5a92eca144845b209226b737b6b9c32c0054
Mike Evans <mike.evans@numical.com>
2017-03-31 21:29:59 +0100

    update set up payment message

--- spec/commands/setUpCreditCardPayment.spec.js
+++ spec/commands/setUpCreditCardPayment.spec.js
@@ -31,7 +31,7 @@ describe('set up credit card command', () => {
   });
 
   it('returns  message', async () => {
-    const expected = 'Ok, I will notify you about any outstanding credit card repayment around the 35th of the month.';
+    const expected = 'Ok, I will notify you about any outstanding credit card repayment around the 35th of the month. In the meantime you can check your [balance].';
     await subject(replyContext, state).should.eventually.equal(expected);
   });
 
